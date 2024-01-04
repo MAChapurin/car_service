88e3e455f15c5f6e04b9a5ebd18faa366c5c7f51
Michael <machapurin@icloud.com>
2024-01-04 15:15:54 +0300

    first commit

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import styles from './footer.module.css'
+
+export function Footer() {
+  return (
+    <div className={styles.wrap}>
+      <footer className={styles.footer}>
+        footer
+      </footer>
+    </div>
+  )
+}
