3762e0d240db7979268b87fb19619e54fb6250ca
Michael <machapurin@icloud.com>
2024-01-19 17:23:55 +0300

    added hover-styles to servicesLinkes

--- src/components/ServicesLinkList/ServicesLinkItem/ServicesLinkItem.tsx
+++ src/components/ServicesLinkList/ServicesLinkItem/ServicesLinkItem.tsx
@@ -3,24 +3,22 @@ import { ServicesLinkProps } from '../ServicesLinkList.props';
 import Image from 'next/image';
 
 import styles from './ServicesLinkItem.module.css'
-// import { Suspense } from 'react';
+
 
 export function ServicesLinkItem({ title, id, img }: ServicesLinkProps) {
   return (
-    // <Suspense fallback={<p>Loading...</p>}>
-      <Link
-        className={styles.link}
-        href={`/services/${id}`}>
-        <Image
-          className={styles.img}
-          src={img}
-          alt={title}
-          width={335}
-          height={200} />
-        <h4>
-          {title}
-        </h4>
-      </Link>
-    // </Suspense>
+    <Link
+      className={styles.link}
+      href={`/services/${id}`}>
+      <Image
+        className={styles.img}
+        src={img}
+        alt={title}
+        width={335}
+        height={200} />
+      <h4 className={styles.h4}>
+        {title}
+      </h4>
+    </Link>
   )
 }
