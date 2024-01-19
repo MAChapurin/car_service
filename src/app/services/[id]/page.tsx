a66a9672cd774011ecfbc8e98a17b692369643be
Michael <machapurin@icloud.com>
2024-01-19 14:39:38 +0300

    created detailedServicesPage

---
+++ src/app/services/[id]/page.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+
+import { detailsServices } from "@/constants";
+
+import cn from 'classnames'
+
+import styles from './DetailServicesPage.module.css'
+import Link from 'next/link';
+
+
+export default function DetailServicePage({ params }: { params: { id: string } }) {
+  const detailService = detailsServices.find(service => service.id === params.id)
+
+  if (!detailService) {
+    return (
+      <p>Страница не найдена</p>
+    )
+  }
+
+  const { title, img, description } = detailService
+
+  return (
+    <div>
+      <h1 className={styles.h1}>{title}</h1>
+      <div className={styles.content}>
+        <Image
+          alt={title}
+          className={styles.img}
+          src={img}
+          width={700}
+          height={400}
+        />
+        <p className={styles.description}>
+          {description}
+        </p>
+      </div>
+      <div className={styles.links}>
+        <Link className={cn(styles.link, styles.link__subscribe)} href={'/'}>Оформить услугу</Link>
+        <Link className={cn(styles.link)} href={'/services'}>Посмотреть другие услуги</Link>
+      </div>
+    </div>
+  )
+}
--- src/components/ServicesLinkList/ServicesLinkItem/ServicesLinkItem.tsx
+++ src/components/ServicesLinkList/ServicesLinkItem/ServicesLinkItem.tsx
@@ -3,21 +3,24 @@ import { ServicesLinkProps } from '../ServicesLinkList.props';
 import Image from 'next/image';
 
 import styles from './ServicesLinkItem.module.css'
+// import { Suspense } from 'react';
 
 export function ServicesLinkItem({ title, id, img }: ServicesLinkProps) {
   return (
-    <Link
-      className={styles.link}
-      href={id}>
-      <Image
-        className={styles.img}
-        src={img}
-        alt={title}
-        width={335}
-        height={200} />
-      <h4>
-        {title}
-      </h4>
-    </Link>
+    // <Suspense fallback={<p>Loading...</p>}>
+      <Link
+        className={styles.link}
+        href={`/services/${id}`}>
+        <Image
+          className={styles.img}
+          src={img}
+          alt={title}
+          width={335}
+          height={200} />
+        <h4>
+          {title}
+        </h4>
+      </Link>
+    // </Suspense>
   )
 }
