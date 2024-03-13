c924ed8aa794f99270d5891d2762b52d3e33fa4e
Mikhail Chapurin <machapurin@icloud.com>
2024-03-13 11:22:05 +0300

    create droppdown into services on mobile

--- src/UI/TabBar/TabBar.tsx
+++ src/UI/TabBar/TabBar.tsx
@@ -2,25 +2,57 @@
 
 import cn from 'classnames'
 import styles from './TabBar.module.css'
+import { Button } from '..'
+import { useState } from 'react'
 
 export function TabBar({ callback, className, list, sortType }: TabBarProps) {
+  const [btnText, setBtntext] = useState('Все')
+
   return (
-    <ul className={cn(styles.ul, className)}>
-      {list.map((el) => {
-        return (
-          <li key={el.title}>
-            <button
-              className={cn(styles.btn, {
-                [styles.active]: sortType === el.type
-              })}
-              data-type={el.type}
-              onClick={callback}
-            >
-              {el.title}
-            </button>
-          </li>
-        )
-      })}
-    </ul>
+    <>
+      <ul className={cn(styles.ul, className)}>
+        {list.map((el) => {
+          return (
+            <li key={el.title}>
+              <button
+                className={cn(styles.btn, {
+                  [styles.active]: sortType === el.type
+                })}
+                data-type={el.type}
+                onClick={callback}
+              >
+                {el.title}
+              </button>
+            </li>
+          )
+        })}
+      </ul>
+      <div className={styles.dropdown}>
+
+        <Button className={styles.dropdownBtn}>
+          {btnText}
+        </Button>
+        <ul className={cn(styles.ulCol, className)}>
+          {list.map((el) => {
+            return (
+              <li key={el.title}>
+                <button
+                  className={cn(styles.btn, {
+                    [styles.active]: sortType === el.type
+                  })}
+                  data-type={el.type}
+                  onClick={(e) => {
+                    callback(e)
+                    setBtntext(el.title)
+                  }}
+                >
+                  {el.title}
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </>
   )
 }
